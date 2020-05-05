918a59c1554c71d23fad467d92b7caf670e0c826
RezaRasoulzadeh <reza.rassoulzadeh@gmail.com>
2020-05-05 23:54:26 +0430

    create base of the app.

---
+++ app/src/main/java/ir/rezarasoulzadeh/zekr/ZekrActivity.kt
@@ -0,0 +1,55 @@
+package ir.rezarasoulzadeh.zekr
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in [ZekrActivityConfigureActivity]
+ */
+class ZekrActivity : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (appWidgetId in appWidgetIds) {
+            deleteTitlePref(context, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = loadTitlePref(context, appWidgetId)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.zekr_activity)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
