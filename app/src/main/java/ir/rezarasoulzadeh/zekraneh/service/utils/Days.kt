9f43181a465eacd7f592298a57e60e5dd272c857
RezaRasoulzadeh <reza.rassoulzadeh@gmail.com>
2020-05-19 18:04:21 +0430

    fix date bugs.

--- app/src/main/java/ir/rezarasoulzadeh/zekraneh/service/utils/Days.kt
+++ app/src/main/java/ir/rezarasoulzadeh/zekraneh/service/utils/Days.kt
@@ -1,26 +1,25 @@
 package ir.rezarasoulzadeh.zekraneh.service.utils
 
-import java.text.SimpleDateFormat
 import java.util.*
 
 class Days {
 
     fun getToday() : String {
-        val simpleDateFormat = SimpleDateFormat("EEEE")
-        val date = Date()
-        return simpleDateFormat.format(date)
+        val c = Calendar.getInstance()
+        val dayOfWeek = c[Calendar.DAY_OF_WEEK]
+        return dayOfWeek.toString()
     }
 
     fun getTodayName(day : String) : String {
         return when (day) {
-            "Saturday" -> "شنبه"
-            "Sunday" -> "یکشنبه"
-            "Monday" -> "دوشنبه"
-            "Tuesday" -> "سه شنبه"
-            "Wednesday" -> "چهارشنبه"
-            "Thursday" -> "پنجشنبه"
-            "Friday" -> "جمعه"
-            else -> "جمعه"
+            "1" -> "یکشنبه"
+            "2" -> "دوشنبه"
+            "3" -> "سه شنبه"
+            "4" -> "چهارشنبه"
+            "5" -> "پنجشنبه"
+            "6" -> "جمعه"
+            "7" -> "شنبه"
+            else -> "شنبه"
         }
     }
 
